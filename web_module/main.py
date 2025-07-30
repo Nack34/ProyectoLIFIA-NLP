5c51f48f7defa438d23bdf37076b0d32afb52edf
Nack34 <gmnack9@gmail.com>
2025-07-30 12:07:35 -0300

    Hello World

--- web_module/main.py
+++ web_module/main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 @app.get("/invertir_texto/")
 def invertir_texto(texto: str):
     texto_invertido = texto[::-1]
-    return {"respuesta": texto_invertido}
+    return {"original": texto, "respuesta": texto_invertido}
+
+
+@app.get("/")
+def default():
+    return invertir_texto("Hello World")
